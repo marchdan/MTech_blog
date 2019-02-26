d360f7cdf2ebeca7cf47d48fbfdd3549b44a51e3
Daniel Ackermans <ackerd2@rpi.edu>
2019-02-26 10:29:25 -0500

    Repo cleaning

--- code/mingus_test.py
+++ code/mingus_test.py
@@ -3,6 +3,8 @@ from mingus.core import *
 from mingus.containers import *
 #from mingus.midi import *
 from mingus.extra import *
+from mingus.midi import *
+from time import *
 '''
 notes.is_valid_note(note)
 notes.reduce_accidentals(note)
@@ -45,8 +47,8 @@ def writemusic(nts, time, key, file):
 			t + b 
 			b = get_bar(time, key)
 		bt += 1
-	test = lilypond.from_Track(t)
-	lilypond.to_pdf(test, file)
+	##test = lilypond.from_Track(t)
+	##lilypond.to_pdf(test, file)
 	return t
 
 if __name__ == "__main__":
@@ -61,6 +63,7 @@ if __name__ == "__main__":
 			fq = raw_input("File?(y/n): ")
 			if fq == 'y':
 				fname = raw_input("File name: ")
+				fname = "inputs/"+fname
 				file = open(fname, "r")
 				NoteL = []
 				key = file.readline().strip()
@@ -80,6 +83,7 @@ if __name__ == "__main__":
 				NoteL = []
 				key = raw_input("Key (Case sensitive): ")
 				transform(key)
+				time = int(input("Beats per measure(#/4): "))
 				num = int(input("Number of notes: "))
 				for i in range(num):
 					get= raw_input("Note(NAME OCTAVE): ").split()
@@ -88,13 +92,32 @@ if __name__ == "__main__":
 					get[0] = transform(get[0])
 					n = get[0] + '-' + get[1]
 					NoteL.append(n)
-				time = int(input("Beats per measure(#/4): "))
 				t = writemusic(NoteL, time, key,raw_input("File Title: "))
+
 		c = Composition()
 		c.add_track(t)
 		c.set_author("Daniel Ackermans", "ackerd2@rpi.edu")
 		c.set_title("Composition")
+
+		t = localtime(None)
+		tS = struct_time(t)
+		time_string = "%s_%s_%s" % (tS[0], tS[1], t[2])
+
+
+		outFile = raw_input("Outfile name: ")
+		pdfF = "pdfs/"+outFile+".pdf"
+		midF = "midiFiles/"+outFile+".mid"
+		file_string = "logs/history_" + time_string + ".txt"
+
 		tet = lilypond.from_Composition(c)
-		lilypond.to_pdf(tet, "yay")
-		print tet
+		lilypond.to_pdf(tet, pdfF)
+		
+		hist = open(file_string, "a")
+		hist.write(str(asctime(t)) + "\n")
+		hist.write(tet + "\n")
+		hist.write("\n")
+		hist.close()
+		
+		midi_file_out.write_Composition(midF, c)
+
 		choice = int(input("Transpose(0) or Write(1)? (-1 to exit) "))
