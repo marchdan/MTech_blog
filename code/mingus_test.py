9120ce2dbf88b2c5d79b7aa291ac07018ad7fb5b
Daniel Ackermans <ackerd2@rpi.edu>
2019-02-26 12:04:47 -0500

    Updates1204

--- code/mingus_test.py
+++ code/mingus_test.py
@@ -21,7 +21,7 @@ def transform(note):
 	if notes.is_valid_note(note):
 		return note
 	else:
-		raise Exception("Please enter a valid musical note!!!!!!")
+		raise Exception("Please enter a valid musical note: " + note)
 
 def transpose(note, trans):
 	if trans > 0:
@@ -37,8 +37,8 @@ def get_bar(time, key):
 	b = Bar(key, (time, 4))
 	return b
 
-def writemusic(nts, time, key, file):
-	t = Track()
+def writemusic(nts, time, key, inst):
+	t = Track(inst)
 	bt = 0
 	b = get_bar(time, key)
 	for i in range(len(nts)):
@@ -52,57 +52,58 @@ def writemusic(nts, time, key, file):
 	return t
 
 if __name__ == "__main__":
+	c = Composition()
 	choice = int(input("Transpose(0) or Write(1)? (-1 to exit) "))
 	while(choice >= 0):
-		if choice == 0:
-			n = raw_input("Note name: ")
-			n = transform(n)
-			t = int(raw_input("Number of half-steps to transpose: "))
-			print transpose(n, t)
-		elif choice == 1:
-			fq = raw_input("File?(y/n): ")
-			if fq == 'y':
-				fname = raw_input("File name: ")
-				fname = "inputs/"+fname
-				file = open(fname, "r")
-				NoteL = []
-				key = file.readline().strip()
-				transform(key)
-				num = int(file.readline().strip())
-				for i in range(num):
-					get= file.readline().strip().split()
-					if (len(get) == 1):
-						get.append("4")
-					get[0] = transform(get[0])
-					n = get[0] + '-' + get[1]
-					NoteL.append(n)
-				time = int(file.readline().strip())
-				t = writemusic(NoteL, time, key, file.readline().strip())
-				file.close()
-			else:
-				NoteL = []
-				key = raw_input("Key (Case sensitive): ")
-				transform(key)
-				time = int(input("Beats per measure(#/4): "))
-				num = int(input("Number of notes: "))
-				for i in range(num):
-					get= raw_input("Note(NAME OCTAVE): ").split()
-					if (len(get) == 1):
-						get.append("4")
-					get[0] = transform(get[0])
-					n = get[0] + '-' + get[1]
-					NoteL.append(n)
-				t = writemusic(NoteL, time, key,raw_input("File Title: "))
+		# if choice == 0:
+		# 	n = raw_input("Note name: ")
+		# 	n = transform(n)
+		# 	t = int(raw_input("Number of half-steps to transpose: "))
+		# 	print transpose(n, t)
+		i = MidiInstrument()
+		i.midi_instr = 69
+		fq = raw_input("File?(y/n): ")
+		if fq == 'y':
+			fname = raw_input("File name: ")
+			fname = "inputs/"+fname
+			file = open(fname, "r")
+			NoteL = []
+			key = file.readline().strip()
+			transform(key)
+			time = int(file.readline().strip())
+			num = int(file.readline().strip())
+			for i in range(num):
+				get= file.readline().strip().split()
+				if (len(get) == 1):
+					get.append("4")
+				get[0] = transform(get[0])
+				n = get[0] + '-' + get[1]
+				NoteL.append(n)
+			t = writemusic(NoteL, time, key, i)
+			c.add_track(t)
+			file.close()
+		else:
+			NoteL = []
+			key = raw_input("Key (Case sensitive): ")
+			transform(key)
+			time = int(input("Beats per measure(#/4): "))
+			num = int(input("Number of notes: "))
+			for i in range(num):
+				get= raw_input("Note(NAME OCTAVE): ").split()
+				if (len(get) == 1):
+					get.append("4")
+				get[0] = transform(get[0])
+				n = get[0] + '-' + get[1]
+				NoteL.append(n)
+			t = writemusic(NoteL, time, key, i)
+			c.add_track(t)
 
-		c = Composition()
-		c.add_track(t)
 		c.set_author("Daniel Ackermans", "ackerd2@rpi.edu")
 		c.set_title("Composition")
 
-		t = localtime(None)
-		tS = struct_time(t)
+		tr = localtime(None)
+		tS = struct_time(tr)
 		time_string = "%s_%s_%s" % (tS[0], tS[1], t[2])
-
 
 		outFile = raw_input("Outfile name: ")
 		pdfF = "pdfs/"+outFile+".pdf"
@@ -113,11 +114,14 @@ if __name__ == "__main__":
 		lilypond.to_pdf(tet, pdfF)
 		
 		hist = open(file_string, "a")
-		hist.write(str(asctime(t)) + "\n")
+		hist.write(str(asctime(tr)) + "\n")
 		hist.write(tet + "\n")
 		hist.write("\n")
 		hist.close()
-		
-		midi_file_out.write_Composition(midF, c)
+
+		# m = midi_file_out.MidiFile([t])
+		# m.write_file(midF)
+		midi_file_out.write_Composition(midF, c, 112)
+
 
 		choice = int(input("Transpose(0) or Write(1)? (-1 to exit) "))
